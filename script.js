3dd5a6a25ec2fc2f5edd5c634402ce8a0662e6c4
soul-eater04 <parthshresth04.k@gmail.com>
2023-12-08 16:41:00 +0530

    do things

---
+++ script.js
@@ -0,0 +1,43 @@
+function addFlexbox(number){
+    for(let i = 0 ; i < number ; i++){
+        const container = document.querySelector('.container');
+        container.style.flexDirection = 'column';
+        const bigBox = document.createElement('div');
+        bigBox.style.display = 'flex';
+        bigBox.style.flexGrow = '1';
+        container.appendChild(bigBox);
+        for(let j = 0 ; j < number ; j++){
+            const smallBox = document.createElement('div');
+            smallBox.addEventListener('click',(e) => color(e))
+            smallBox.style.backgroundColor = 'white';
+            smallBox.style.border = "1px dashed grey";
+            smallBox.style.flexGrow = '1';
+            bigBox.appendChild(smallBox);
+        }
+    }
+}
+
+function clear(){
+    const container = document.querySelector('.container');
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    } 
+}
+
+function color(e){
+    const box = e.target;
+    if(box.style.backgroundColor === 'white'){
+        box.style.backgroundColor = 'black';
+    }
+    else{
+        box.style.backgroundColor = 'white';
+    }
+}
+
+addFlexbox(25);
+const btn = document.querySelector('button');
+btn.addEventListener('click',() =>{
+    let size = prompt("ENTER THE SIZE OF GRID");
+    clear();
+    addFlexbox(size);
+});
